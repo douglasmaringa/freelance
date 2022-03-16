74ad67701fb81ec8cbe5be3dce6ba50307819df2
Douglas Maringa <douglasmaringa4@gmail.com>
2022-03-16 23:03:53 +0200

    Updates

---
+++ precache-manifest.cc94505faf7c1d82a3471afaaefb7371.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182282a5b4b60e85ca287342937d7c7e",
+    "url": "/freelance/index.html"
+  },
+  {
+    "revision": "a57420f0b2088212d8bd",
+    "url": "/freelance/static/css/main.1013169e.chunk.css"
+  },
+  {
+    "revision": "04abf6953ff855f5bcda",
+    "url": "/freelance/static/js/2.7b09c6c8.chunk.js"
+  },
+  {
+    "revision": "a57420f0b2088212d8bd",
+    "url": "/freelance/static/js/main.007af698.chunk.js"
+  },
+  {
+    "revision": "a3cbf3aba6661bb97f5f",
+    "url": "/freelance/static/js/runtime-main.9cc61471.js"
+  },
+  {
+    "revision": "5f86af978913654b7f6279eb19782687",
+    "url": "/freelance/static/media/like.5f86af97.png"
+  },
+  {
+    "revision": "eb77bfb55efc0021e68a6186ce1cadc1",
+    "url": "/freelance/static/media/p1.eb77bfb5.jpg"
+  },
+  {
+    "revision": "d13486a103ab741205326db427566332",
+    "url": "/freelance/static/media/p2.d13486a1.jpg"
+  },
+  {
+    "revision": "2d13453bfed9b8622b5e2abd7399d7c9",
+    "url": "/freelance/static/media/p3.2d13453b.jpg"
+  },
+  {
+    "revision": "b33b9181020763573debbb34c3920778",
+    "url": "/freelance/static/media/p4.b33b9181.jpg"
+  },
+  {
+    "revision": "b4ea08bb3b40e600dbe457608289ccb3",
+    "url": "/freelance/static/media/p5.b4ea08bb.jpg"
+  },
+  {
+    "revision": "57b8821639388c70be74a4d8a3a54ff7",
+    "url": "/freelance/static/media/p6.57b88216.jpg"
+  },
+  {
+    "revision": "4164b7fa32d18a065900748edc6c4b09",
+    "url": "/freelance/static/media/shape2.4164b7fa.png"
+  }
+]);
